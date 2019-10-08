0107c5082f9a326dc708c4b56ba19ecf963dd302
Jock Lawrie <jock.lawrie@gmail.com>
2019-10-08 14:34:36 +1100

    Added kwarg sorted_by_primarykey.

--- src/compare_data_to_schema.jl
+++ src/compare_data_to_schema.jl
@@ -33,9 +33,10 @@ There are 2 methods for comparing a table to a schema:
    It examines one row at a time.
    The 3 tables of results (see above) are stored on disk. By default they are stored in the same directory as the input table.
 """
-compare(tableschema::TableSchema, table) = compare_inmemory_table(tableschema, table)
+compare(tableschema::TableSchema, table; sorted_by_primarykey=false) = compare_inmemory_table(tableschema, table, sorted_by_primarykey)
 
-function compare(tableschema::TableSchema, input_data_file::String; output_data_file="", input_issues_file="", output_issues_file="")
+function compare(tableschema::TableSchema, input_data_file::String;
+                 output_data_file="", input_issues_file="", output_issues_file="", sorted_by_primarykey=false)
     !isfile(input_data_file) && error("The input data file does not exist.")
     fname, ext = splitext(input_data_file)
     output_data_file   = output_data_file   == "" ? "$(fname)_transformed.tsv"   : output_data_file
@@ -43,20 +44,21 @@ function compare(tableschema::TableSchema, input_data_file::String; output_data_
     output_issues_file = output_issues_file == "" ? "$(fname)_output_issues.tsv" : output_issues_file
     outdir  = dirname(output_data_file)  # outdir = "" means output_data_file is in the pwd()
     outdir != "" && !isdir(outdir) && error("The directory containing the specified output file does not exist.")
-    compare_ondisk_table(tableschema, input_data_file, output_data_file, input_issues_file, output_issues_file)
+    compare_ondisk_table(tableschema, input_data_file, output_data_file, input_issues_file, output_issues_file, sorted_by_primarykey)
 end
 
 ################################################################################
 # compare_inmemory_table
 
-function compare_inmemory_table(tableschema::TableSchema, indata)
+function compare_inmemory_table(tableschema::TableSchema, indata, sorted_by_primarykey)
     # Init
     tablename     = tableschema.name
     outdata       = init_outdata(tableschema, size(indata, 1))
     issues_in     = init_issues(tableschema)  # Issues for indata
     issues_out    = init_issues(tableschema)  # Issues for outdata
     pk_colnames   = tableschema.primarykey
-    primarykey    = fill("", length(pk_colnames))  # Stringified primary key
+    pk_ncols      = length(pk_colnames)
+    primarykey    = missings(String, pk_ncols)  # Stringified primary key 
     pkvalues_in   = Set{String}()  # Values of the primary key
     pkvalues_out  = Set{String}()
     i_outdata     = 0
@@ -73,20 +75,32 @@ function compare_inmemory_table(tableschema::TableSchema, indata)
         parserow!(outputrow, inputrow, colname2colschema)
 
         # Assess input row
-        if length(pk_colnames) > 1  # The uniqueness of 1-column primary keys is checked at the column level
-            populate_primarykey!(primarykey, pk_colnames, inputrow)
-            pkvalue = join(primarykey)
-            primarykey_isunique!(issues_in, pkvalues_in, pkvalue)
+        if pk_ncols > 1  # The uniqueness of 1-column primary keys is checked at the column level
+            if sorted_by_primarykey
+                if primarykey_is_duplicated!(primarykey, pk_colnames, inputrow)
+                    issues_in[:primarykey_duplicates] += 1
+                end
+            else
+                populate_primarykey!(primarykey, pk_colnames, inputrow)
+                pkvalue = join(primarykey)
+                primarykey_isunique!(issues_in, pkvalues_in, pkvalue)
+            end
         end
         assess_row!(issues_in[:columnissues], inputrow, colname2colschema, uniquevalues_in)
         nconstraints > 0 && test_intrarow_constraints!(issues_in[:intrarow_constraints], tableschema, inputrow)
 
         # Assess output row
         assess_row_set_invalid_to_missing!(issues_out[:columnissues], outputrow, colname2colschema, uniquevalues_out)
-        if length(pk_colnames) > 1
-            populate_primarykey!(primarykey, pk_colnames, outputrow)
-            pkvalue = join(primarykey)
-            primarykey_isunique!(issues_out, pkvalues_out, pkvalue)
+        if pk_ncols > 1
+            if sorted_by_primarykey
+                if primarykey_is_duplicated!(primarykey, pk_colnames, outputrow)
+                    issues_out[:primarykey_duplicates] += 1
+                end
+            else
+                populate_primarykey!(primarykey, pk_colnames, outputrow)
+                pkvalue = join(primarykey)
+                primarykey_isunique!(issues_out, pkvalues_out, pkvalue)
+            end
         end
         nconstraints > 0 && test_intrarow_constraints!(issues_out[:intrarow_constraints], tableschema, outputrow)
     end
@@ -109,18 +123,19 @@ end
 ################################################################################
 # compare_ondisk_table
 
-function compare_ondisk_table(tableschema::TableSchema, input_data_file, output_data_file, input_issues_file, output_issues_file)
+function compare_ondisk_table(tableschema::TableSchema, input_data_file, output_data_file, input_issues_file, output_issues_file, sorted_by_primarykey)
     # Init
     tablename     = tableschema.name
     outdata       = init_outdata(tableschema, input_data_file)
     issues_in     = init_issues(tableschema)  # Issues for indata
     issues_out    = init_issues(tableschema)  # Issues for outdata
     pk_colnames   = tableschema.primarykey
-    primarykey    = fill("", length(pk_colnames))  # Stringified primary key 
+    pk_ncols      = length(pk_colnames)
+    primarykey    = missings(String, pk_ncols)  # Stringified primary key 
     pkvalues_in   = Set{String}()  # Values of the primary key
-    pkvalues_out  = Set{String}()
     i_outdata     = 0
     nconstraints  = length(tableschema.intrarow_constraints)
+    pk_colnames_set   = Set(pk_colnames)
     colname2colschema = tableschema.colname2colschema
     uniquevalues_in   = Dict(colname => Set{colschema.datatype}() for (colname, colschema) in colname2colschema if colschema.isunique==true)
     uniquevalues_out  = Dict(colname => Set{colschema.datatype}() for (colname, colschema) in colname2colschema if colschema.isunique==true)
@@ -142,10 +157,16 @@ function compare_ondisk_table(tableschema::TableSchema, input_data_file, output_
         parserow!(outputrow, inputrow, colname2colschema)
 
         # Assess input row
-        if length(pk_colnames) > 1  # The uniqueness of 1-column primary keys is checked at the column level
-            populate_primarykey!(primarykey, pk_colnames, inputrow)
-            pkvalue = join(primarykey)
-            primarykey_isunique!(issues_in, pkvalues_in, pkvalue)
+        if pk_ncols > 1  # The uniqueness of 1-column primary keys is checked at the column level
+            if sorted_by_primarykey
+                if primarykey_is_duplicated!(primarykey, pk_colnames, inputrow)
+                    issues_in[:primarykey_duplicates] += 1
+                end
+            else
+                populate_primarykey!(primarykey, pk_colnames, inputrow)
+                pkvalue = join(primarykey)
+                primarykey_isunique!(issues_in, pkvalues_in, pkvalue)
+            end
         end
         assess_row!(colissues_in, outputrow, colname2colschema, uniquevalues_in)  # Note: outputrow used because inputrow contains only Strings
         nconstraints > 0 && test_intrarow_constraints!(issues_in[:intrarow_constraints], tableschema, outputrow)
@@ -153,7 +174,7 @@ function compare_ondisk_table(tableschema::TableSchema, input_data_file, output_
         # Assess output row
         # For speed, avoid testing value_is_valid directly. Instead reuse assessment of input.
         # Testing intra-row constraints is unnecessary because either outputrow hasn't changed or the tests return early due to missingness
-        ischanged = false  # True if at least 1 value in outputrow is changed to missing
+        pk_has_changed = false
         for (colname, colschema) in colname2colschema
             val = outputrow[colname]
             ci  = colissues_out[colname]
@@ -164,34 +185,31 @@ function compare_ondisk_table(tableschema::TableSchema, input_data_file, output_
                     end
                 else
                     if colschema.isunique
-                        if in(val, uniquevalues_out[colname])
+                        uniquevals = uniquevalues_out[colname]
+                        if in(val, uniquevals)
                             ci[:n_notunique] += 1
                         else
-                            push!(uniquevalues_out[colname], val)
+                            push!(uniquevals, val)
                         end
                     end
                 end
             else  # input value (=output value) is invalid...set to missing and report as missing, not as invalid
-                ischanged = true
                 @inbounds outputrow[colname] = missing
+                pk_has_changed = pk_ncols > 1 && !pk_has_changed && in(colname, pk_colnames_set)
                 if colschema.isrequired
                     ci[:n_missing] += 1
                 end
             end
         end
-        if length(pk_colnames) > 1  # Single-column primary keys are assessed via columnissues[colname][:n_notunique]
-            if ischanged
-                populate_primarykey!(primarykey, pk_colnames, outputrow)  # Only necessary if outputrow has changed
-                pkvalue = join(primarykey)
-            end
-            primarykey_isunique!(issues_out, pkvalues_out, pkvalue)
+        if pk_ncols > 1 && !pk_has_changed  # Single-column primary keys are assessed via columnissues[colname][:n_notunique]
+            issues_out[:primarykey_duplicates] += 1
         end
 
         # If outdata is full append it to output_data_file
         i_outdata != n_outdata && continue
-        i_outdata  = 0  # Reset the row number
-        nr        += n_outdata
         CSV.write(output_data_file, outdata; append=true, delim=delim_outdata)
+        nr        += n_outdata
+        i_outdata  = 0  # Reset the row number
     end
     if i_outdata != 0
         CSV.write(output_data_file, view(outdata, 1:i_outdata, :); append=true, delim=delim_outdata)
@@ -258,10 +276,41 @@ function compare_datatypes!(issues, table, colname2colschema)
 end
 
 "Modified: primarykey"
-function populate_primarykey!(primarykey::Vector{String}, pk_colnames::Vector{Symbol}, row)
+function populate_primarykey!(primarykey::Vector{Union{String, Missing}}, pk_colnames::Vector{Symbol}, row)
     for (j, colname) in enumerate(pk_colnames)
-        @inbounds primarykey[j] = string(getproperty(row, colname))
+        val = getproperty(row, column)
+        if ismissing(val)
+            @inbounds primarykey[j] = missing
+        else
+            @inbounds primarykey[j] = string(val)
+        end
     end
+end
+
+"""
+Modified: pk_prev
+
+Compares the current row's primary key to the previous row's primary key.
+Returns true if the 2 primary keys are the same.
+Also populates pk_prev with the current row's primary key (after doing the comparison).
+
+- If a value is missing then the comparison returns false (primary key is not duplicated).
+"""
+function primarykey_is_duplicated!(pk_prev::Vector{Union{String, Missing}}, pk_colnames::Vector{Symbol}, currentrow)
+    result = true
+    for (j, colname) in enumerate(pk_colnames)
+        if result == true
+            val = getproperty(currentrow, colname)
+            @inbounds prev_val = pk_prev[j]
+            if ismissing(val) || ismissing(prev_val) || val != pk_prev[j]
+                result = false
+            end
+            @inbounds pk_prev[j] = val
+        else
+            @inbounds pk_prev[j] = getproperty(currentrow, colname)
+        end
+    end
+    result
 end
 
 "Modified: Either issues or pkvalues."
